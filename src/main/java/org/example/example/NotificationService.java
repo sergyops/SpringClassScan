fad6bd80c0dcde4d3bb846e1a6e0541e1afe3a4e
sergyops <70977028+sergyops@users.noreply.github.com>
2022-12-05 12:27:39 +0100

    Initial commit

---
+++ src/main/java/org/example/example/NotificationService.java
@@ -0,0 +1,11 @@
+package org.example.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService {
+
+    public void helloWorld(){
+        System.out.println("Hello World :)");
+    }
+}
---
+++ src/main/java/org/example/example/UserService.java
@@ -0,0 +1,13 @@
+package org.example.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    NotificationService notificationService;
+
+    public UserService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+}
